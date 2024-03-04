b884a4dd6991ddb85dfc3d85843ae93ccb2cb0fc
aravinds13 <aravindtvm13@gmail.com>
2024-03-04 07:48:02 -0500

    clean up code

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const mongoDBClient = require('mongodb').MongoClient;
-
 const mongoose = require('mongoose');
 const budgetModel = require('./models/budget_schema');
 
@@ -15,56 +13,6 @@ const url = 'mongodb://localhost:27017'
 
 app.use(cors());
 app.use(express.json());
-// mongoose.connect(url)
-//     .then(() => {
-//         console.log("success");
-//         budgetModel.insertMany({
-//             title: 'Hello',
-//             budget: 20,
-//             backgroundColor: '#ffffff'
-//         })
-//             .then((data) => {
-//                 console.log(data);
-//             })
-//             .catch((error) => {
-//                 console.log(error);
-//             })
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-    
-    // mongoose.connect(url)
-    // .then(() => {
-    //     console.log("success");
-    //     budgetModel.find({})
-    //         .then((data) => {
-    //             console.log(data);
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         })
-    // })
-    // .catch((err) => {
-    //     console.log(err);
-    // });
-
-// const budget = {
-//     myBudget: [
-//         {
-//             title: 'Eat out',
-//             budget: 25
-//         },
-//         {
-//             title: 'Rent',
-//             budget: 275
-//         },
-//         {
-//             title: 'Grocery',
-//             budget: 110
-//         },
-//     ]
-// };
 
 
 app.get('/budget', (req, res) => {
